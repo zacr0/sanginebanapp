2c3fd29adf091bd6cf9b91625d24a7abe6e8bf3b
zacr0 <zacro.suarez@gmail.com>
2014-04-15 00:53:42 +0200

    Added intento de carga de valores

--- SangineBapp/gen/com/example/sanginebapp/R.java
+++ SangineBapp/gen/com/example/sanginebapp/R.java
@@ -17,8 +17,8 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int banhammer_normal=0x7f020000;
@@ -27,23 +27,26 @@ public final class R {
         public static final int ic_launcher=0x7f020003;
     }
     public static final class id {
-        public static final int action_settings=0x7f080004;
-        public static final int btn_banear=0x7f080003;
-        public static final int layout=0x7f080000;
-        public static final int texto_contador=0x7f080002;
-        public static final int titulo_contador=0x7f080001;
+        public static final int action_settings=0x7f090004;
+        public static final int btn_banear=0x7f090003;
+        public static final int layout=0x7f090000;
+        public static final int texto_contador=0x7f090002;
+        public static final int titulo_contador=0x7f090001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
+    }
+    public static final class raw {
+        public static final int punch=0x7f040000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int texto_titulo=0x7f050002;
-        public static final int txt_contador=0x7f050003;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int texto_titulo=0x7f060002;
+        public static final int txt_contador=0x7f060003;
     }
     public static final class style {
         /** 
@@ -67,10 +70,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
--- SangineBapp/src/com/example/sanginebapp/MainActivity.java
+++ SangineBapp/src/com/example/sanginebapp/MainActivity.java
@@ -1,19 +1,37 @@
 package com.example.sanginebapp;
 
 import android.app.Activity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+	TextView contador;
 	Button btnBanear;
-	
+	MediaPlayer sndBoton;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
+		contador = (TextView) findViewById(R.id.texto_contador);
 		btnBanear = (Button) findViewById(R.id.btn_banear);
+		sndBoton = MediaPlayer.create(this, R.raw.punch);
+
+		btnBanear.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				sndBoton.start();
+				postBans(null);
+			}
+		});
+		
+		getBans(null);
 	}
 
 	@Override
@@ -21,6 +39,14 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	public void getBans(View view) {
+		new BansActivity(this).execute("sangine");
+	}
+	
+	public void postBans(View view) {
+		
 	}
 
 }
